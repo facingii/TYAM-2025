09b22bb0f5cdfe320bece8d34b2d9e6570bde921
Gonzalo Sosa <gonzaloantonio@gmail.com>
2025-10-09 17:45:20 -0600

    Gallery app without using specialized libraries

---
+++ Intermediate/GallerySoloApp/app/build.gradle.kts
@@ -0,0 +1,47 @@
+plugins {
+    alias(libs.plugins.android.application)
+}
+
+android {
+    namespace = "mx.uv.fiee.iinf.tyam.gallerysoloapp"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "mx.uv.fiee.iinf.tyam.gallerysoloapp"
+        minSdk = 29
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("com.squareup.okhttp3:okhttp:5.2.0")
+
+    implementation(libs.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.ext.junit)
+    androidTestImplementation(libs.espresso.core)
+
+}
