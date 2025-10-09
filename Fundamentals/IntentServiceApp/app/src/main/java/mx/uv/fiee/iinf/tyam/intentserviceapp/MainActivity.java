869c1cfa9071e2cd24ba538c670943fef106f1e6
Gonzalo Sosa <gonzaloantonio@gmail.com>
2025-10-09 17:43:59 -0600

    Background service using workers

---
+++ Fundamentals/IntentServiceApp/app/src/main/java/mx/uv/fiee/iinf/tyam/intentserviceapp/MainActivity.java
@@ -0,0 +1,9 @@
+package mx.uv.fiee.iinf.tyam.intentserviceapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+}
---
+++ Fundamentals/IntentServiceApp/app/src/main/java/mx/uv/fiee/iinf/tyam/intentserviceapp/MyIntentService.java
@@ -0,0 +1,17 @@
+package mx.uv.fiee.iinf.tyam.intentserviceapp;
+
+
+import android.content.Intent;
+
+import androidx.annotation.Nullable;
+
+import kotlinx.coroutines.scheduling.CoroutineScheduler;
+
+public class MyWorker extends CoroutineScheduler.Worker {
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+
+    }
+
+}
